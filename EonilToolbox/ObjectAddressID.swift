a4cd5efd44d3113be704824201b0f95a8bdd0998
Hoon H <drawtree@me.com>
2016-05-13 10:59:41 +0900

    Rename and update `ObjectID` to `ObjectAddressID`.

--- EonilToolbox/ObjectAddressID.swift
+++ EonilToolbox/ObjectAddressID.swift
@@ -1,31 +1,40 @@
 //
-//  ObjectID.swift
+//  ObjectAddressID.swift
 //  EonilToolbox
 //
 //  Created by Hoon H. on 2016/04/27.
 //  Copyright © 2016 Eonil. All rights reserved.
 //
 
-public struct ObjectID: Hashable {
+/// An ID that is based on object address and unique in current process scope.
+///
+/// - Note:
+///     Current implementation is kind of naive, consumes memory uselessly.
+///
+public struct ObjectAddressID: Hashable {
     private let dummy = Dummy()
     public init() {
-
     }
     public var hashValue: Int {
         get { return ObjectIdentifier(dummy).hashValue }
     }
-}
-extension ObjectID: CustomStringConvertible {
-    public var description: String {
-        get { return "0x" + String(format: "%X", ObjectIdentifier(dummy).uintValue) }
+    /// Same ID always returns same pointer value.
+    /// Nothing is guaranteed about what actually is at pointed address. 
+    public func asObject() -> AnyObject {
+        return dummy
     }
 }
-extension ObjectID: CustomDebugStringConvertible {
+extension ObjectAddressID: CustomStringConvertible {
+    public var description: String {
+        get { return "(ObjectID: 0x" + String(format: "%X", ObjectIdentifier(dummy).uintValue) + ")" }
+    }
+}
+extension ObjectAddressID: CustomDebugStringConvertible {
     public var debugDescription: String {
         get { return description }
     }
 }
-public func ==(a: ObjectID, b: ObjectID) -> Bool {
+public func ==(a: ObjectAddressID, b: ObjectAddressID) -> Bool {
     return a.dummy === b.dummy
 }
 
