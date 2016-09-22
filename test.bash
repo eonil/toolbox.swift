1440586affdac5b65f94ce5c053c189550b19ab5
Hoon H <drawtree@gmail.com>
2016-09-23 01:07:45 +0900

    Add iOS test target, remove code signing and update test script.

--- test.bash
+++ test.bash
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-xcodebuild -scheme EonilToolbox-iOS -sdk iphonesimulator10.0 build
-xcodebuild -scheme EonilToolbox-OSX build test
+xcodebuild -scheme EonilToolbox-iOS -configuration Debug -sdk iphonesimulator10.0 clean build test
+xcodebuild -scheme EonilToolbox-iOS -configuration Release -sdk iphonesimulator10.0 clean build
+
+xcodebuild -scheme EonilToolbox-OSX -configuration Debug clean build test
+xcodebuild -scheme EonilToolbox-OSX -configuration Release clean build
+
