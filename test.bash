cf477445464e0feebd6353b53b15e425f784f5fc
Hoon H <drawtree@gmail.com>
2016-09-23 00:51:34 +0900

    Update Travis test script and configuration.

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-xcodebuild -scheme EonilToolbox-iOS -sdk iphonesimulator build
+xcodebuild -scheme EonilToolbox-iOS -sdk iphonesimulator10.0 build
 xcodebuild -scheme EonilToolbox-OSX build test
