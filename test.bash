cb06193d3b4715151869982f1a1ab24f70e868ec
Hoon H <dev@eonil.com>
2017-01-15 20:22:33 +0900

    Fix test script to use any random available simulator.

--- test.bash
+++ test.bash
@@ -2,7 +2,8 @@
 set -e errexit
 set -o pipefail
 
-IOS_DEST="platform=iOS Simulator,name=iPhone 6,OS=10.2"
+#IOS_DEST="platform=iOS Simulator,name=iPhone 6,OS=latest"
+IOS_DEST=id=`instruments -s devices | grep "iPhone" | grep "Simulator" | tail -1 | grep -o "\[.*\]" | tr -d "[]"`
 xcodebuild -scheme EonilToolbox-iOS -destination "$IOS_DEST" -configuration Debug clean build test
 xcodebuild -scheme EonilToolbox-iOS -destination "$IOS_DEST" -configuration Release clean build
 
