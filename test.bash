9990f43a2265bffa8cb1c73ca4d65f60f143eb6a
Hoon H <dev@eonil.com>
2017-01-15 19:33:36 +0900

    Degrade test target model due to lack of model image in Travis machine.

--- test.bash
+++ test.bash
@@ -2,7 +2,7 @@
 set -e errexit
 set -o pipefail
 
-IOS_DEST="platform=iOS Simulator,name=iPhone 7,OS=10.2"
+IOS_DEST="platform=iOS Simulator,name=iPhone 6,OS=10.2"
 xcodebuild -scheme EonilToolbox-iOS -destination "$IOS_DEST" -configuration Debug clean build test
 xcodebuild -scheme EonilToolbox-iOS -destination "$IOS_DEST" -configuration Release clean build
 
